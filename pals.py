0b60232bd6d4ecdfdbd0e3bccbebd1fb26464f26
tommymmcguire <141086024+tommymmcguire@users.noreply.github.com>
2023-09-11 18:14:14 +0000

    add visualization

--- pals.py
+++ pals.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
+csv_url = "https://raw.githubusercontent.com/paiml/wine-ratings/main/wine-ratings.csv"
+df = pd.read_csv(csv_url, index_col=0)
 
 def data_desc(csv_url):
     df = pd.read_csv(csv_url, index_col=0)
@@ -6,3 +10,10 @@ def data_desc(csv_url):
     return df['rating'].count()
 # print(df.describe())
 
+
+plt.hist(df['rating'], bins=20, edgecolor='k')
+plt.xlabel('Wine Ratings')
+plt.ylabel('Frequency')
+plt.title('Distribution of Wine Ratings')
+plt.grid(True)
+plt.savefig("wine_rating.png", dpi=300)
